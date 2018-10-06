2f046f4762dd9d1bddcb130fafe7b302e07f16d7
[aa279662529] <[279662529@qq.com]>
2018-10-07 00:00:18 +0800

    Initial commit

---
+++ proxypool/scheduler.py
@@ -0,0 +1,52 @@
+import time
+from multiprocessing import Process
+from .setting import *
+from .tester import Tester
+from .getter import Getter
+from .api import app
+
+class Scheduler():
+    def schedule_tester(self,cycle=TESTER_CYCLE ):
+        """
+        定时测试代理
+        :param cycle:
+        :return:
+        """
+        tester = Tester()
+        while True:
+            print('测试器开始运行')
+            tester.run()
+            time.sleep(cycle)
+
+    def scheduler_getter(self,cycle=GETTER_CYCLE):
+        """
+        定时获取代理
+        :param cycle:
+        :return:
+        """
+        getter = Getter()
+        while True:
+            print('开始抓取代理')
+            getter.run()
+            time.sleep(cycle)
+
+    def schedule_api(self):
+        """
+        开启API
+        """
+        app.run(API_HOST, API_PORT)
+
+    def run(self):
+        print('代理池开始运行')
+
+        if TESTER_ENABLED:
+            tester_process = Process(target=self.schedule_tester)
+            tester_process.start()
+
+        if GETTER_ENABLED:
+            getter_process = Process(target=self.scheduler_getter)
+            getter_process.start()
+
+        if API_ENABLED:
+            api_process = Process(target=self.schedule_api)
+            api_process.start()
